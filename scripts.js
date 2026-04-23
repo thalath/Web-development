b8734a8dd981db2104629e0e1f58bcf7e8cd7033
Thalath Tun <tunthalath@gmail.com>
2026-04-23 17:25:16 +0700

    first commit

--- scripts.js
+++
@@ -1,9 +0,0 @@
-const active = document.querySelectorAll('.navbar ul li');
-
-active.forEach(links => {
-    links.addEventListener('click', function(){
-        active.forEach(x => x.classList.remove('active'));
-
-        this.classList.add('active');
-    });
-});
